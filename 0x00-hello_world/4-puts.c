e4d60072f02d5bba22ab4fa77104de73442b7ee4
Ernestphilips <ernest1philips@gmail.com>
2022-06-30 10:32:19 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 # include <stdio.h>
-int main - Entry point
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
