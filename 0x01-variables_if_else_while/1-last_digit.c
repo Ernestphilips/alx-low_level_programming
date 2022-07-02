35a260243e4ab5d3e83d4efb84801450b1c81dcd
Ernestphilips <ernest1philips@gmail.com>
2022-07-01 17:46:02 -0700

    alphabets

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,6 +14,7 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+
 printf("Last digit of %d is ", n);
 if (n > 5)
 {
@@ -21,13 +22,12 @@ printf("greater than 5");
 }
 if (n == 0)
 {
-printf("0")
+printf("0");
 }
 if (n < 6 && n != 0)
 {
 printf("less than 6 not 0");
 }
 printf("\n");
-
 return (0);
 }
