8675c3e5610a78d0604f6d7508f8ed41c76edc97
Ernestphilips <ernest1philips@gmail.com>
2022-07-03 13:58:25 -0700

    print combo

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,12 +14,13 @@ for (n = 49; n <= 57; n++)
 {
 if (n > ch)
 {
-putchar(ch);
-putchar(n);
+	int putchar(int ch);
+	int putchar(int n);
+
 if (ch != 56 || ch != 57)
 {
-putchar(44);
-Putchar(32);
+	putchar(44);
+	putchar(32);
 }
 }
 }
