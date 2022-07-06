b0ef740d5fb56c9e473bc0a49d6f794ce802d28b
Ernestphilips <ernest1philips@gmail.com>
2022-07-06 04:40:35 +0000

    hotfix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,10 +10,10 @@ int _print_sign(int n)
 
 {
 	if (n > 0)
-	{
-		_putchar('+');
-		return (1);
-	}
+{
+	_putchar('+');
+	return (1);
+}
 else if (n == 0)
 {
 	_putchar('0');
--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,11 @@
 
 int print_last_digit(int n)
 {
-	int x;
+	int x = n % 10;
 
 	if (n < 0)
-	n = -n;
-	x = n % 10;
+	x *= -1;
+
 	_putchar(x + '0');
 	return (x);
 }
