7f032da7a745e446551a769df22798017ba2a48a
Ernestphilips <ernest1philips@gmail.com>
2022-07-06 02:16:02 +0000

    functions

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
+ * main - write a function that prints the sign of a number
+ * @n: args
  * Return: 1, 0, -1
  */
 
-
-int main(void); int _print_sign(int n)
+int _print_sign(int n);
 
 {
 	if (n > 0)
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include  "main.h"
+
+/**
+ * print_last_digit ->  print the last digit
+ * @n: the passed args
+ * Return: 0 if succesful
+ */
+
+int print_last_digit(int n)
+{
+	int y;
+
+if	(n < 0)
+	n = -n;
+	y =  n % 10;
+	_putchar(y + '0');
+return (y);
+}
