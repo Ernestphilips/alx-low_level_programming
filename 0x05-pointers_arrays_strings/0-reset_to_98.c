679c725afa20e8cdbc4620bc0bebaedf6b0156c9
Ernestphilips <ernest1philips@gmail.com>
2022-07-11 18:21:00 +0000

    hot fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,11 @@
 #include "main.h"
 
 /**
- * main - a function that takes a pointer to an int as parameter
+ * reset_to_98 - a function that takes a pointer to an int as parameter
  * @n: points n to the update.
  */
 
 void reset_to_98(int *n)
 {
 	*n = 98;
-
 }
