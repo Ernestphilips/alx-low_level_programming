5387a10852e28514b160997edfa63f4e0637bc42
Ernestphilips <ernest1philips@gmail.com>
2022-08-01 17:11:32 +0000

    task 1

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef OBJECT_LIKE_MACRO_H
+#define OBJECT_LIKE_MACRO_H
+
+/**
+ * file: 0-object_like_macro.h
+ * author: Ernest Philips
+ * work: defines a macro SIZE as abbreviation for 1024
+ */
+
+#define SIZE 1024
+
+#endif
