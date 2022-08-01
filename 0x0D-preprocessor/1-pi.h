df8b51d514bcfe3bf95d8695d67d981fecb175d7
Ernestphilips <ernest1philips@gmail.com>
2022-08-01 17:19:55 +0000

    task 1

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,12 @@
+#ifndef PI_H
+#define PI_H
+
+/**
+ * file: 1-pi.h
+ * author: Ernest Philips
+ * work: defines a macro PI as abbreviation for 3.14159265359
+ */
+
+#define PI 3.14159265359
+
+#endif
