fad296767780dabe630f3453ecc56d526d589402
Ernestphilips <ernest1philips@gmail.com>
2022-08-02 17:24:34 +0000

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,7 @@ struct dog
 /**
  *  * dog_t - Typedef for struct dog
  *   */
-typedef struct dog dog_t;
+struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
