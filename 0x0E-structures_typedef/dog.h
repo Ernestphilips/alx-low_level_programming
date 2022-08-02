a4aa9535cf72d66fd4f83176b3b0edcab725a01c
Ernestphilips <ernest1philips@gmail.com>
2022-08-02 12:33:31 +0000

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,11 +2,6 @@
 #define DOG_H
 
 /**
- * file: dog.h is a header file.
- * desc: header file that defines a new type struct dog
- */
-
-/**
  * struct dog - a new type describing a dog
  * @name: name of dog
  * @owner: name of owner
