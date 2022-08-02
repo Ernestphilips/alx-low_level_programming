afadfea6c059dd1045a1a5879e2ce635eb3bb336
Ernestphilips <ernest1philips@gmail.com>
2022-08-02 23:43:59 +0000

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - freee dog
+ * @d: dog to be freed
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
