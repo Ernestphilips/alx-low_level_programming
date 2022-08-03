6741bc11f15adb01750d1110b3e5f370290bcc11
Ernestphilips <ernest1philips@gmail.com>
2022-08-03 23:58:40 +0000

    task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -11,10 +11,10 @@ int (*get_op_func(char *s))(int, int)
 {
 	op_t ops[] = {
 		{"+", op_add},
-		{"-", op_sub};
-		{"*", op_mul};
-		{"/", op_div};
-		{"%", op_mod};
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
 		{NULL, NULL}
 	};
 	int i = 0;
