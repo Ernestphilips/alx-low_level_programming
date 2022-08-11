1af1dafe713a30c8b3e62d39a3543626d08d9c21
Ernestphilips <ernest1philips@gmail.com>
2022-08-11 23:34:28 +0000

    task 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * hare - prints string before main
+ * function is executed
+ */
+void hare(void)
+{
+	printf("You're beat! and yet.you must allow,\n"
+		"I bore my house upon my  back!\n");
+}
