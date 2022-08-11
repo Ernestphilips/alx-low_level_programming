0ab9bb6977c2ce65227e0cfc0ab552c488f3317a
Ernestphilips <ernest1philips@gmail.com>
2022-08-11 23:57:07 +0000

    corrected output

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,5 @@
 void hare(void)
 {
 	printf("You're beat! and yet.you must allow,\n"
-		"I bore my house upon my  back!\n");
+			"I bore my house upon my  back!\n");
 }
--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -4,21 +4,16 @@
 
 
 	extern printf
-
 	section .text
-		global main
+	global main
 
-	main:
-		push rbp
-
-	mov rdi, fmt
-	mov rsi, msg
-	mov rax,0
+main:
+	mov edi, fmt
+	mov esi, msg
+	mov eax, 0
 	call printf
 
-	pop rbp
-
-	mov rax,0
+	mov eax, 0
 	ret
 
 
