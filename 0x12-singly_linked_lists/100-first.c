4b9cf51f5316cda2b4e47c269a46cfdc6b11853a
Ernestphilips <ernest1philips@gmail.com>
2022-08-17 03:21:13 +0000

    fixed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@ void __attribute__((constructor)) hare(void);
  */
 void hare(void)
 {
-	printf("You're beat! and yet.you must allow,\n"
+	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my  back!\n");
 }
