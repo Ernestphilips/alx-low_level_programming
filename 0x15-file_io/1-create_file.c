7cf9c041b1234a81d5b00fb4b5cd94aa5d4d4547
Ernestphilips <ernest1philips@gmail.com>
2022-08-22 18:50:32 +0000

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	}
 
 	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o,text_content, len);
+	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
 		return (-1);
