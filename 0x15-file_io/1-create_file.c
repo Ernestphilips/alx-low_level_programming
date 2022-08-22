25adcfddf618b19765a8a73b676f86aa06314a46
Ernestphilips <ernest1philips@gmail.com>
2022-08-22 18:49:04 +0000

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - function that creates a file
+ * @filename: pointer to the name of the file
+ * @text_content: pointer to a string to write
+ * Return: 1 on success, -1  on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o,text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
