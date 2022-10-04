b8b48afe2140b4e8521fecfdea49b2f0d1345577
Ernest <ernest1philips@gmail.com>
2022-10-04 04:42:42 +0000

    hot fix

--- 0x18-dynamic_libraries/0-main.c
+++ 0x18-dynamic_libraries/0-main.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - check the code
@@ -8,6 +9,6 @@
  */
 int main(void)
 {
-    printf("%d\n", _strlen("My Dyn Lib"));
-    return (EXIT_SUCCESS);
+	printf("%d\n", _strlen("My Dyn Lib"));
+	return (EXIT_SUCCESS);
 }
